e0f30a3b564970fa874538707aa0e689db6cd160
hous3m4ster <hous3m4ster@protonmail.com>
2016-10-30 21:44:11 +0100

    :heart:

--- index.js
+++ index.js
@@ -5,3 +5,10 @@
  *
  */
 'use strict';
+const token = "<token>"; //insert token obtained through @botfather
+const telebot = require("telebotframework").TelegramBot;
+const bot = new telebot(token);
+bot.startLongPolling();
+bot.on('message', msg => {
+    msg.reply("Hi, I am peoplefinder\nI love finding people"); //test case
+});
