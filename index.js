ab915da0b0e272b3db0dd7bba701684a569f314f
hous3m4ster <hous3m4ster@protonmail.com>
2016-10-30 22:35:49 +0100

    <insert message>

--- index.js
+++ index.js
@@ -9,6 +9,21 @@ const token = "<token>"; //insert token obtained through @botfather
 const telebot = require("telebotframework").TelegramBot;
 const bot = new telebot(token);
 bot.startLongpolling();
-bot.on('message', msg => {
-    msg.reply("Hi, I am peoplefinder\nI love finding people"); //test case
+bot.on('text', msg => {
+        if (msg.text.indexOf('/') === 0) {
+            let commandArgs = msg.text.split(/\s+/);
+            let command = commandArgs.shift();
+            command = command.substr(1).split('@')[0];
+            command = command.toLowerCase()
+            let commandText = commandArgs.join(' ');
+            let text = '';
+            switch(command){
+                case 'findperson':
+                case 'findpeople':
+                case 'findpeepz':
+                case 'findsomeone':
+                    msg.reply(msg.from.id);
+                    break;
+            }
+        }
 });
